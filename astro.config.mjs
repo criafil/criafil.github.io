d90902d3df78cbba2e18fac673267e719cc64fb1
criafil <criafil.sde@gmail.com>
2026-03-23 23:30:41 +0200

    Vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: 'https://criafil.github.io',
   base: '/criafil.github.io',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
